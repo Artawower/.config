2907f3d4e9abfaa0bd8d24a50cd18d0c29ca8b87
artawower <artawower@mail.ru>
2023-10-18 04:20:05 +0400

    lsp rename action

--- nvim/lua/cmp_config.lua
+++ nvim/lua/cmp_config.lua
@@ -16,6 +16,7 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-x C-o>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
