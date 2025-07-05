57e92d9a6bc27ec7fba7df8212fea121e64ff519
Art <Artawower@mail.ru>
2025-07-05 09:20:23 +0500

    backup wezterm

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -28,7 +28,7 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
     buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
     buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 end
 
 -- Setup lspconfig.
@@ -37,7 +37,7 @@ end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = {"ts-ls", "stylelint_lsp", "cssls", "pyright", "vuels", "gopls"}
+local servers = {"ts_ls", "stylelint_lsp", "cssls", "pyright", "vuels", "gopls"}
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         capabilities = capabilities,
