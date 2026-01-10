cceb95f3f0469ef20381f4fc88eac213364e5431
Art <Artawower@mail.ru>
2026-01-10 16:55:57 +0400

    move emacs to config directory

---
+++ emacs/init.el
@@ -0,0 +1,10 @@
+;;; init.el --- Emacs configuration entry point -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; This file loads the main configuration from README_NEW.org
+
+;;; Code:
+(require 'org)
+(org-babel-load-file "~/config/emacs/README.org")
+
+;;; init.el ends here
